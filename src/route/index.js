5033df946a484cfbff60b2d637d21e16ea580da8
Valeria Savienkova <metra89@protonmail.com>
2024-02-13 19:06:20 +0100

    edit

--- src/route/index.js
+++ src/route/index.js
@@ -1117,6 +1117,18 @@ router.get('/bootstrap1', function (req, res) {
 
 // ================================================================
 
+router.get('/bootstrap2', function (req, res) {
+  // res.render генерує нам HTML сторінку
+
+  //            ↙ cюди вводимо назву файлу з сontainer
+  res.render('bootstrap2', {
+    layout: 'bootstrap',
+  })
+  //                  ↑↑ сюди вводимо JSON дані
+})
+
+// ================================================================
+
 router.get('/bootstrap3', function (req, res) {
   // res.render генерує нам HTML сторінку
 
